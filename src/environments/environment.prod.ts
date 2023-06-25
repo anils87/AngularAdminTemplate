04ba210a0336b7c868a1b4fecf75241554ae94b0
Anil Yadav <anils15787@gmail.com>
2023-06-25 12:29:18 +0530

    api url updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
     API_URL:'http://localhost:4200/',
-    ProductAPI_URL:'https://anilapi001.azurewebsites.net/'
+    ProductAPI_URL:'https://projtemplateapi20230625122218.azurewebsites.net/'
   };
